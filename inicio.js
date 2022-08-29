6ff82ef875b607deae88dedceecd1a0e5c647dff
marianacgonzalez <107015276+marianacgonzalez@users.noreply.github.com>
2022-08-29 18:13:55 -0200

    Add files via upload

--- inicio.js
+++ inicio.js
@@ -1,47 +1,57 @@
-let nombre= prompt ("Ingrese su nombre")
-bienvenida ();
 
-function bienvenida() { alert( "Bienvenido" + " " +
-    nombre + " " + "¡¡Iniciemos la compra!!"
-)
+let nombre= prompt  ("Ingrese su nombre")
+let apellido= prompt ("Ingrese su apellido")
+bienvenida()
+
+
+function bienvenida() { alert( `Bienvenido ${nombre} ${apellido} ¡¡Iniciemos la compra!!`)
 }
 
 let opciones=  prompt("Elije una remera: \n 1: Remera Pink \n 2: Remera Black \n 3: No quiero comprar")
 
-if (opciones==="1") {
-    remerapink()
-}
-else if (opciones==="2") {
-    remerablack()
-}
-
-else if (opciones==="3") {
-    alert("Te esperamos la proxima")
+while(opciones != 3){
+    if (opciones==="1") {
+        remerapink()
+        break
+    }
+    else if (opciones==="2") {
+        remerablack()
+        break
+    }
+    
+    else if (opciones==="3") {
+        alert("Te esperamos la proxima")
+    }else {
+        opciones = prompt("Opcion incorrecta, vuelva a intentarlo. \n 1: Remera Pink \n 2: Remera Black \n 3: No quiero comprar")
+    }
 }
 
 function remerapink() { alert("Elegiste Remera Pink $2000")
-    
-}
-function remerablack() { alert("Elegiste Remera Black $2000")
-    
-}
-
 let comprar= prompt("¿Como deseas abonar? \n 1:Tarjeta \n2: Efectivo(10%off)")
-
-if (comprar==="1") {
-    tarjeta()
+if (comprar==="1") { alert("Abonaste con tarjeta, Gracias por tu compra :)")
 }
 else if(comprar==="2"){
-    alert("Elegiste efectivo presiona aceptar para ver tu %:")
+    alert("Elegiste efectivo, presiona aceptar para ver tu %:")
+    let total= Number(alert("Tu descuento es de $ "+ 2000*10/100+" "+ "Gracias por tu compra, te esperamos la próxima"))
 }
 
-function tarjeta() { alert("Felicidades Abonaste con tarjeta")
-    
+}
+function remerablack() { alert("Elegiste Remera Black $2000")
+let comprar= prompt("¿Como deseas abonar? \n 1:Tarjeta \n2: Efectivo(10%off)")
+if (comprar==="1") { alert("Abonaste con tarjeta, Gracias por tu compra, te esperamos la próxima")
+}
+else if(comprar==="2"){
+    alert("Elegiste efectivo presiona aceptar para ver tu descuento:")
+    let total= Number(alert("Tu descuento es de $" + 2000*10/100 + "Gracias por tu compra, te esperamos la próxima"))
+}
 }
 
-let total= Number(alert("Tu descuento es de $" + 2000*10/100))
 
-alert("Gracias por tu compra, te esperamos la próxima")
+
+
+
+
+
 
 
 
